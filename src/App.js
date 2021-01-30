612802fce3e5e9e7de17b2761eb07425466fdbeb
pro <pro@macbook-pro-de-mathieu.home>
2021-01-30 19:47:36 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import randomText from './randomText'
 
 function App() {
+  const [textEntered, setTextEntered] = useState('')
+
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setTextEntered(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='container'>
+        <p className="sub-header">TYPING SPEED TEST</p>
+        <h1 className="title">Test your typing skills</h1>
+
+        {/* <label>Enter value : </label>
+        <textarea
+          value={modelText}
+          onChange={handleChange}
+          rows={5}
+          cols={5} */}
+        {/* /> */}
+        <div className="wrapper-input">
+          <input
+            className="input-text"
+            value={textEntered}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+    </div >
   );
 }
 
